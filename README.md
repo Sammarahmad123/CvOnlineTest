a681733b7e68f27e3da4a6799f1591cb48b4ae14
sammarah <sammarah@ut.ee>
2024-05-10 19:47:00 +0300

    aspectj added, readme added and E2E test updated with steps

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# CvOnlineTest
+# CV.ee Test Automation Project
+
+## Overview
+
+This project automates the end-to-end testing of the CV.ee registration process using Java, Maven, TestNG, Selenium, and Allure for reporting.
+
+## Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Project Structure](#project-structure)
+- [Setup Instructions](#setup-instructions)
+- [Running Tests](#running-tests)
+- [Generating Allure Reports](#generating-allure-reports)
+- [Configuration](#configuration)
+- [Additional Information](#additional-information)
+
+## Prerequisites
+
+- **Java 17** or higher
+- **Maven 3.6.3** or higher
+- **Web Browser** (Chrome)
+- **IDE** (IntelliJ IDEA, Eclipse, etc.)
+
+
+## Setup Instructions
+
+### Clone the Repository
+
+#### Clone the project and then run following command to run the test cases
+```bash
+mvn clean install
+mvn clean test -> this command will run the tests
+mvn allure:serve -> this will generate alure reports and opens a new browser window to view report
--- src/test/java/ui/Registration/E2E/RegistrationE2E.java
+++ src/test/java/ui/Registration/E2E/RegistrationE2E.java
@@ -3,6 +3,7 @@ package ui.Registration.E2E;
 import base.BaseTest;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
+import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -18,6 +19,10 @@ public class RegistrationE2E extends BaseTest {
     private HomePage homePage;
     private RegistrationPage registrationPage;
     private LoginPage loginPage;
+    private final String validEmail = EmailGenerator.generateRandomEmail();
+    private final String password = "ValidPassword123";
+    private final String firstName = "John";
+    private final String lastName = "Doe";
 
     @BeforeClass
     public void setUpTest() {
@@ -29,15 +34,35 @@ public class RegistrationE2E extends BaseTest {
     @Test(description = "Test Successful Registration with Valid Data")
     @Description("Test Description: Ensure that a user can register successfully with valid data.")
     public void testRegistrationWithValidDetails() {
+        step1();
+        step2();
+        step3();
+        step4();
+        step5();
+
+    }
+    @Step("Step 1: Click on Login|Registration Button")
+    public void step1(){
         homePage.clickLoginRegister();
+    }
+    @Step("Step 2: Go To Registration Page as a job seeker with email registration")
+    public void step2(){
         registrationPage.goToRegistrationPageWithRegisterAsJobSeekerWIthEmail();
-        String validEmail = EmailGenerator.generateRandomEmail();
-        String password = "ValidPassword123";
-        String firstName = "John";
-        String lastName = "Doe";
+    }
+    @Step("Step 3: Register New user")
+    public void step3(){
         registrationPage.registerNewUser(validEmail, password, firstName, lastName);
+    }
+    @Step("Step 4: Verify that successful registration message is shown")
+    public void step4(){
         Assert.assertTrue(loginPage.isSuccessfulRegistrationMessageShown(), "Successful registration message is not displayed.");
+    }
+    @Step("Step 5: Click Login Button")
+    public void step5(){
         loginPage.clickLoginButton();
+    }
+    @Step("Step 6: Verify that Login Form is Shown")
+    public void step6(){
         Assert.assertTrue(loginPage.isLoginFormShown(), "Successful registration message is not displayed.");
     }
 }
